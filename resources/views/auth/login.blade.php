1a9d1b2c240808432d45140f217c3f833bb14c97
CarlosJavierML <71906724+CarlosJavierML@users.noreply.github.com>
2022-12-01 19:20:20 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -109,12 +109,12 @@
             
                 </div>
                 <div class="mb-3 form-password-toggle">
-                {{--  <div class="d-flex justify-content-between">
+                  <div class="d-flex justify-content-between">
                     <label class="form-label" for="password">Contraseña</label>
                     <a href="auth-forgot-password-basic.html">
-                      <small>¿Olvido la contraseña?</small>
+                     {{-- <small>¿Olvido la contraseña?</small>--}}
                     </a>
-                  </div> --}}
+                  </div> 
                   <div class="input-group input-group-merge">
                     <input
                       type="password"
